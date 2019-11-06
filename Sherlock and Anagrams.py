4814cd9bbc487de28ea20423ec749fd55f53801e
kk-karankatyal10 <57436825+kk-karankatyal10@users.noreply.github.com>
2019-11-06 14:51:54 +0530

    Create Sherlock and Anagrams.py

---
+++ Sherlock and Anagrams.py
@@ -0,0 +1,15 @@
+def sherlockAndAnagrams(s):
+    n = len(s)
+    res = 0
+    for l in range(1, n):
+        cnt = {}
+        for i in range(n - l + 1):
+            subs = list(s[i:i + l])
+            subs.sort()
+            subs = ''.join(subs)
+            if subs in cnt:
+                cnt[subs] += 1
+            else:
+                cnt[subs] = 1
+            res += cnt[subs] - 1
+    return res

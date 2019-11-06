8e2acd49fe47c654e71566f4fa5708bb0e325c25
Karan Katyal <57436825+kk-karankatyal10@users.noreply.github.com>
2019-11-06 18:31:40 +0530

    Create Matrix Script.py

---
+++ Matrix Script.py
@@ -0,0 +1,18 @@
+import re
+
+# Import original script
+matrix = list()
+for _ in range(int(input().split()[0])):
+    matrix.append(list(input()))
+
+# Rotate the matrix
+matrix = list(zip(*matrix))
+
+# Prep regex sample
+sample = str()
+for subset in matrix:
+    for letter in subset:
+        sample += letter
+
+# Substitute invalid characters with a space
+print(re.sub(r'(?<=\w)([^\w\d]+)(?=\w)', ' ', sample))

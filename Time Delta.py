1a8e21f3db5e1f5239008e15c41820f1e8036b07
Karan Katyal <57436825+kk-karankatyal10@users.noreply.github.com>
2019-11-06 18:15:47 +0530

    Create Time Delta.py

---
+++ Time Delta.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    time_format = '%a %d %b %Y %H:%M:%S %z'
+    t1 = datetime.strptime(t1, time_format)
+    t2 = datetime.strptime(t2, time_format)
+    return str(int(abs((t1-t2).total_seconds())))

321462b49e484733a0d1ec4ddef45b66efdd53e1
kk-karankatyal10 <57436825+kk-karankatyal10@users.noreply.github.com>
2019-11-06 14:53:23 +0530

    Create Count Triplets.py

---
+++ Count Triplets.py
@@ -0,0 +1,14 @@
+def countTriplets(arr, r):
+    m1, m2 = defaultdict(int), defaultdict(int)
+    triplets = 0
+
+    for i in reversed(arr):
+        if (i * r) in m2:
+            triplets += m2[i * r]
+
+        if (i * r) in m1:
+            m2[i] += m1[i * r]
+
+        m1[i] += 1
+
+    return triplets

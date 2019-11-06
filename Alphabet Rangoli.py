aa6b1a727e6939743aab673ccfa4ff9d9eb10fed
Karan Katyal <57436825+kk-karankatyal10@users.noreply.github.com>
2019-11-06 16:59:05 +0530

    Update Alphabet Rangoli.py

--- Alphabet Rangoli.py
+++ Alphabet Rangoli.py
@@ -6,3 +6,15 @@ def print_rangoli(size):
     for i in range(size-1):
         s = "-".join(chr(ord('a')+size-j-1) for j in range(size-i-1))
         print((s+s[::-1][1:]).center(size*4-3, '-'))
+        
+'''
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+'''

d0260f04aa7e012e0c3889d86bdd6f2eae5fb1ec
Karan Katyal <57436825+kk-karankatyal10@users.noreply.github.com>
2019-11-07 12:57:34 +0530

    Create Grading Students Sam Rule.py

---
+++ Grading Students Sam Rule.py
@@ -0,0 +1,8 @@
+def gradingStudents(grades):
+    # Write your code here
+    for i in range(len(grades)):
+        if(grades[i]>37):
+            if((grades[i]%5)!=0):
+                if(5-(grades[i]%5)<3):
+                    grades[i]+=5-(grades[i]%5)
+    return (grades)
